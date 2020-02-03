a1792d839fc8fbfb870974f039000726bc859f72
Cameron Reikes <cameronreikes@gmail.com>
2020-02-02 18:58:47 -0800

    Add basic rotating player controller

--- src/Player.gd
+++ src/Player.gd
@@ -1,9 +1,23 @@
 extends KinematicBody
 
-var accel: Vector3 = Vector3(0.0, -9.81, 0.0)
+const move_velocity = 7.0
+
+var accel: float = -9.81
 var velocity: Vector3 = Vector3()
+var rot_velocity: float = 0.0
 
 func _physics_process(delta):
+	rot_velocity = float(Input.is_action_pressed("g_turn_right")) - float(Input.is_action_pressed("g_turn_left"))
+	rot_velocity *= -10.0
 	
-	velocity += accel*delta
+	var input_vector: Vector2 = Vector2(\
+		float(Input.is_action_pressed("g_right")) - float(Input.is_action_pressed("g_left")), \
+		float(Input.is_action_pressed("g_up")) - float(Input.is_action_pressed("g_down")) \
+	).rotated(rotation.y)
+	
+	velocity.x = input_vector.x
+	velocity.z = input_vector.y
+	velocity.y += accel*delta
+	
 	velocity = move_and_slide(velocity, Vector3(0, 1, 0))
+	rotation.y += rot_velocity*delta
